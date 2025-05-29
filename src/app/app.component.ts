1d298275908c8f8d51d94c166f1ffdf33d913a36
chris-scientist <chris.scientist.contact@gmail.com>
2025-05-29 18:20:04 +0200

    Ajout bouton accueil + Option désactiver ou non

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,14 @@ import { RouterOutlet, Router } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1 class="m-2 text-white" (click)="goToHome()">{{title}}</h1>
+    <h1 class="m-2 text-white">
+      {{title}}
+      <button (click)="goToHome()" class="btn btn-light" title="Retourner à l'accueil" [disabled]="isHomeRoute()" >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+          <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+        </svg><!-- Retourner à l'accueil-->
+      </button>
+    </h1>
 
     <router-outlet />
   `,
@@ -14,9 +21,13 @@ import { RouterOutlet, Router } from '@angular/router';
 export class AppComponent {
   title = 'Terrain de jeu Angular 19';
 
-  private router = inject(Router);
+  readonly #router = inject(Router);
 
   goToHome() {
-    this.router.navigate(['/home']);
+    this.#router.navigate(['/home']);
+  }
+
+  isHomeRoute() {
+    return this.#router.url.startsWith("/home");
   }
 }
